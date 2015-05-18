0500b97e5734c6e41666b70782d61c802b1912a9
Riccardo Mereu <riccardomereu@MacBook-Pro-di-Riccardo.local>
2015-05-18 12:08:37 +0200

    fast push

--- Server_working/neverlatewebserver_sperimentale.py
+++ Server_working/neverlatewebserver_sperimentale.py
@@ -7,7 +7,8 @@ from flask import Flask , render_template, request, session, url_for, redirect
 
 app = Flask(__name__)
 app.secret_key='chiavesegreta'
-All_user ={}
+All_user = {}
+errorList = []
 
 class utente:
 	def __init__(self):
@@ -29,6 +30,7 @@ def login():
 @app.route('/loggining', methods=['POST', 'GET'])
 def loggining():
 	global All_user
+	print All_user
 	username=request.form.get('username')
 	password=request.form.get('password')
 	
@@ -42,7 +44,11 @@ def loggining():
 		else:
 			return redirect(url_for('login')+"?valid=PswF")
 	else:
-		return redirect(url_for('login')+"?valid=UsrF")
+		if username == "":
+			return redirect(url_for('login')+"?valid=NoUsr")
+			
+		else:
+			return redirect(url_for('login')+"?valid=UsrF")
 	
 @app.route('/logout')
 def logout():
@@ -57,68 +63,66 @@ def vision():
 def requirements():
     return render_template('requirements.html')
     
-@app.route('/default_user')
+@app.route('/user')
 def default_user():
-	return render_template('default_user.html')
+	if 'user' in session:
+		return render_template('default_user.html')
+		
+	else:
+		return redirect(url_for('login'))
 	
 @app.route('/newuser', methods=['POST', 'GET'])
 def newuser():
 	global All_user
+	global errorList
 	
 	email=request.form.get('email')
 	email_rep=request.form.get('email_rep')
-	
-	if (email != email_rep):
-		redirect ( url_for('registration')+"?valid=repMailF")
-		
-	
 	password=request.form.get('password')
 	password_rep=request.form.get('password_rep')
-	
-	if (password != password_rep):
-		redirect ( url_for('registration')+"?valid=repPswF")
-		
-		
 	username=request.form.get('username')
 	
+	errorList=[]
+	error=False
+	
+	if email == "" or email_rep == "" or email != email_rep:
+		error = True
+		errorList.append('Email field')
+	
+	if password == "" or password_rep == "" or password != password_rep:
+		error = True
+		errorList.append('Password field')
+	
+	if username == "":
+		error = True
+		errorList.append('Username field')
+	
+	if error == True:
+		return redirect(url_for('registration')+"?error=t")
 	
 	user.username=username
 	user.password=password
 	user.email=email
 	
 	if username in All_user:
-		if password == All_user[username].password:
-			session['user']=username
-			print All_user
-			return redirect(url_for('login')+"valid=extUsr")
-			
-		else:
-			return redirect(url_for('registration')+"?valid=exUsr")
+		return redirect(url_for('login')+"?valid=extUsr")
 	
 	else:
 		All_user[username] = user
 		session['user']=username
-		
-		print All_user
 		
 		return redirect(url_for('login'))
 	
 	
 @app.route('/registration', methods=['POST', 'GET'])
 def registration():
-    return render_template('registration.html', validation_login=request.args.get('valid'))
+	
+	global errorList
+	return render_template('registration.html', error=request.args.get('error'), errors=errorList)
     
 @app.route('/architecture')
 def architecture():
 	return render_template('architecture.html')
-
-
-
-#@app.route('/api/data',methods=['GET'])
- #def index():
- #   data=jsonifymysql
- #   return data
-
 
 if __name__ == '__main__':
     user=utente()
